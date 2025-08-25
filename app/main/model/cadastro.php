7b4291d43de76730a47ee0eff7cd6e7705d5d2a7
DenilsonCosta <denilsoncostayt@gmail.com>
2025-08-25 15:34:39 -0300

    Update cadastro.php

--- app/main/model/cadastro.php
+++ app/main/model/cadastro.php
@@ -1,1 +1,24 @@
+<?php
+require_once '../includes/db.php';
 
+if (!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    
+
+    $query = "INSERT INTO usuario (nome, email, senha, TIPO) 
+              VALUES ('$nome', '$email', '$password', null)";
+    
+    $result = mysqli_query($link, $query);
+
+    if ($result) {
+        header("Location: ../view/index.php?sucesso=1");
+        exit;
+    } else {
+        echo "Erro ao cadastrar: " . mysqli_error($link);
+    }
+} else {
+    header("Location: ../view/cadastro.php?erro=1");
+    exit;
+}
