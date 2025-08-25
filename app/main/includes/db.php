41f330bdaf3c098fcb9dc95ce92a9530792546da
DenilsonCosta <denilsoncostayt@gmail.com>
2025-08-25 15:36:55 -0300

    Update db.php

--- app/main/includes/db.php
+++ app/main/includes/db.php
@@ -1,1 +1,26 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=almoxarifado;charset=utf8', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
+$host = 'localhost';
+$usuario = 'root';
+$senha = '' ;
+$nome_banco = 'almoxarifado';
+
+$link = mysqli_connect($host, $usuario, $senha, $nome_banco);
+mysqli_set_charset($link, 'utf8');
+if (!$link) {
+    die("A Conexão falhou:" . mysqli_connect_error());
+}
+mysqli_set_charset($link, 'utf8');
+
+if ($link) {
+    echo "Conexão bem-sucedida ao banco de dados.";
+} else {
+    echo "Erro ao conectar ao banco de dados:" . mysqli_connect_error();
+}
+
+
+date_default_timezone_set('America/Sao_Paulo');
+?>
