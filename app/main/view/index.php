e11967b87a656b3d64a373961cee7dce566290ff
RamonNN-bit <ramonnunes.mendonca@gmail.com>
2025-08-25 13:06:48 -0300

    Update index.php

--- app/main/view/index.php
+++ app/main/view/index.php
@@ -3,31 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style.css">
-    <script src="/js/script.js"></script>
     <title>Login</title>
 
-</head>
 <body>
-    <div>
- 
-    <form action="/login" method="POST">
-   <h2>Login</h2>
-        <label for="username">Usuário:</label>
-        <input type="text" id="username" name="username" required><br>
-        
+    <!-- Login -->
+    <form action="/conexao.php" method="POST">
+    <h2>Login</h2>
+    <div class="container">
+        <label for="email">Email:</label>
+        <input type="email" id="username" name="email" placeholder="nome@gmail.com" required><br>
+    </div> 
+    <div class="password-container">
         <label for="password">Senha:</label>
-        <input type="password" id="password" name="password" required><br>
-        <label for="remember">
-            <input type="checkbox" id="remember" name="remember"> Lembrar-me
-        </label>
-        <a href=""></a>
+        <input type="password" id="password" name="password" placeholder="senha" required><i class="bi-eye-fill" id="mostrarSenhaBtn" onclick="mostrarSenha()"></i></input><br>
+    </div>
+        <label id="remember">
+        <input type="checkbox" id="remember" name="remember">Lembrar-me</input> </label>
+        <a href="senha.php" id="forgotpassword">Esqueceu a Senha</a>
 
         <button type="submit" id="btn">Entrar</button>
     </form>
-    </div>
+
     <footer>
-    <img src="../assets/logo.png" alt="Logo" class="logo">
+    <img src="../assets/logosemfundo.png" alt="Logo" class="logo">
     </footer>
+
 </body>
+<script src="script.js"></script>
 </html>
