0d8282b4608c165647e95ffaba66ed770b236033
DenilsonCosta <denilsoncostayt@gmail.com>
2025-08-25 13:48:24 -0300

    Update dashboard_usuario.php

--- app/main/painel/Usuario/dashboard_usuario.php
+++ app/main/painel/Usuario/dashboard_usuario.php
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Principal</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            margin: 0;
+            padding: 20px;
+        }
+
+header {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 0;
+            text-align: center;
+        }
+        </style>          
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+
+
+
 
