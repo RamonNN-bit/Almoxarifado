a1ac0ef7ee5f535eb4baf3e845a79841a6335859
DenilsonCosta <denilsoncostayt@gmail.com>
2025-08-25 14:08:26 -0300

    Update cadastro.php

--- app/main/view/cadastro.php
+++ app/main/view/cadastro.php
@@ -7,11 +7,11 @@
 </head>
 <body>
     <!-- Cadastro -->
-    <form action="/conexao.php" method="POST">
+    <form action="../model/conexao.php" method="POST">
     <h2>Cadastro</h2>
     <div class="container">
         <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="email" placeholder="Nome" required><br>
+        <input type="text" id="nome" name="nome" placeholder="Nome" required><br>
     </div>
 
     <div class="container">
@@ -26,11 +26,11 @@
     <button type="submit" id="btn">Cadastrar</button>
 
     <?php
-    require_once = '../model/conexao.php';
+    require_once '../model/conexao.php';
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    if(isset($_POST{'nome'}) && isset($_POST['email']) && isset($_POST['password'])){
+    if(isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['password'])){
     $query = "INSERT INTO `usuario`(`id_usuario`, `nome`, `email`, `senha`, `TIPO`) VALUES ('','$nome','$email','$password','$tipo_usuario')";
     $result = mysqli_query($link, $query);
     }else{
