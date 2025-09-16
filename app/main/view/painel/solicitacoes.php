1cd60911c7d732406d19e1c0c787cdda79017a55
Ramon <ramonnunes.mendonca@gmail.com>
2025-09-16 13:21:12 -0300

    Update solicitacoes.php

--- app/main/view/painel/solicitacoes.php
+++ app/main/view/painel/solicitacoes.php
@@ -641,6 +641,7 @@ function getStatusClass($status)
                 if (selectedOption.value) {
                     const quantidade = selectedOption.dataset.quantidade;
                     const unidade = selectedOption.dataset.unidade;
+                    estoqueInfo.textContent = `Estoque disponível: ${quantidade} ${unidade}`;
                     quantidadeInput.max = quantidade;
                 } else {
                     estoqueInfo.textContent = '';
